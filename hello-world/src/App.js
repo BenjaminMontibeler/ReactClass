db5c68a40facdaf6eb08e89021ffbe9ba14298ce
Benjamin Montibeler <benjaminmontibeler@gmail.com>
2023-01-17 17:52:34 +0100

    Lessons 10-16 completed

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,12 +4,33 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message';
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
 
 function App() {
   return (
     <div className="App">
 
-      <Message />
+      <NameList />
+
+      {/* <UserGreeting /> */}
+
+      {/* <ParentComponent /> */}
+
+      {/* <EventBind /> */}
+
+      {/*<FunctionClick />*/}
+
+      {/*<ClassClick />*}
+
+      {/*<Counter />*/}
+
+      {/*<Message />*/}
 
       {/*<Greet name = "Bruce" heroName = "Batman">
         <p>This is children props</p>
